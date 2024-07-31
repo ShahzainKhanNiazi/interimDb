27320459643803c6e596ff36fe7d464b71574f9d
RehanMaroof <rehanmaroof000@gmail.com>
2024-07-31 17:53:53 +0500

    nongoDb connection setup

--- config/db.js
+++ config/db.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
     });
     console.log('MongoDB connected');
   } catch (err) {
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Connect Database
-// connectDB();
+connectDB();
 
 // Routes
 app.get("/", (req, res)=>{
